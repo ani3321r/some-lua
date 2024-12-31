2759673197b513966fe39050f557084c7ce82307
ani3321r <ani3321r@gmail.com>
2024-12-31 17:32:52 +0530

    some os and io modules

--- builtin.lua
+++ builtin.lua
@@ -22,3 +22,31 @@ print(string.find(name, "Raiden")) -- give the indexes of the occuring string
 local start_ch, end_ch = string.find(name, "Raiden") -- can initialize more than one var
 local res = "start char " .. start_ch .. " end char " .. end_ch
 print(res)
+
+
+-- io
+
+io.write("Enter ur name: ")
+local name = io.read()
+
+io.write("Give ur age: ")
+local age = tonumber(io.read())
+
+print("Hello " .. name .. " u were born in " .. tonumber(2024-age))
+
+
+-- os
+
+os.execute("export NAME=Raiden")
+
+local clock = os.clock
+local function sleep(n)
+   local t0 = clock()
+   while clock() - t0 <= n do
+   end
+end
+
+sleep(2)
+
+local name = os.getenv("NAME")
+print("hello, " .. tostring(name))
